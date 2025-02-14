3cf5d90f77dab8ebe26ab43573815d51cd9325af
howardnm <bueess@163.com>
2025-02-14 16:34:08 +0800

    fix Dockerfile

--- sw_BI/settings.py
+++ sw_BI/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'app01/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['bueess.top']
