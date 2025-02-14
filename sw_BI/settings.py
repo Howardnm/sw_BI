619f93dcc8adf2f328bd4e08c5f3635e55c093d9
howardnm <bueess@163.com>
2025-02-14 11:17:47 +0800

    fix Dockerfile

--- sw_BI/settings.py
+++ sw_BI/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/app/app01/static/'
+STATIC_URL = 'app01/static/'
+# STATIC_ROOT = '/app/app01/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
